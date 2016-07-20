3cab0ecc6cfbaafb15d35aa37030acfbd96b1e41
Christian Menz <christianmenz@gmail.com>
2016-07-20 16:55:24 +0200

    added sample sudoku and updated seed to take into account given numbers

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 Genetic = require('genetic-js');
+_ = require('lodash');
 
 var genetic = Genetic.create();
 
@@ -7,13 +8,14 @@ genetic.select1 = Genetic.Select1.Tournament2;
 genetic.select2 = Genetic.Select2.Tournament2;
 
 genetic.seed = function () {
-  var randomSolution = [];
-  for (var i = 0; i < 9; i++) {
-    randomSolution[i] = [];
-    for (var j = 0; j < 9; j++) {
-      randomSolution[i][j] = Math.floor(Math.random()*9+1); 
+  var randomSolution = _.cloneDeep(this.userData.sudoku);
+  for (var i = 0; i < 9; i++) {    
+    for (var j = 0; j < 9; j++) {    
+      if (randomSolution[i][j] === null) {
+        randomSolution[i][j] = Math.floor(Math.random() * 9 + 1);
+      }
     }
-  }  
+  }
   return randomSolution;
 }
 
@@ -51,7 +53,17 @@ var config = {
 };
 
 var userData = {
-
+  sudoku: [
+    [null, null, 6, 3, null, 5, 7, null, null],
+    [null, 9, 2, 7, null, null, null, 4, null],
+    [3, null, null, null, 9, null, null, 2, 5],
+    [8, null, null, null, 6, null, null, 1, 7],
+    [null, null, 7, 5, null, 8, 2, null, null],
+    [9, 2, null, null, 3, null, null, null, 6],
+    [1, 8, null, null, 4, null, null, null, 2],
+    [null, 6, null, null, null, 9, 3, 8, null],
+    [null, null, 5, 8, null, 1, 9, null, null]
+  ]
 };
 
 genetic.evolve(config, userData);
